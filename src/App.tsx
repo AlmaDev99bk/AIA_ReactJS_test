ea7bcb211cdaaf753d508f16014f797b65f0b243
VHai dev <hai.nguyenviet021199@gmail.com>
2022-04-28 08:37:31 +0700

    done UI and data logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
-
-import './App.css';
-
+import { Routes, Route, Navigate } from 'react-router-dom';
+import HomePage from './screens/homePage/HomePage'
+import CartPage from './screens/cartPage/CartPage';
+import ProductPage from './screens/productPage/ProductPage';
+import './App.scss'
+import NavBar from './component/Navbar';
+import logo from './assets/logo.png';
 function App() {
   return (
     <div className="App">
-      <p>abc</p>
+      <NavBar logo={logo}></NavBar>
+      <div className="content">
+        <Routes>
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/cart" element={<CartPage />} />
+          <Route path="/product/:id" element={<ProductPage />} />
+          <Route
+            path="*"
+            element={<Navigate to="/home" replace />}
+          />
+        </Routes>
+
+      </div>
     </div>
   );
 }
